a10162b2514b324b99a6a4dad4134b1e965b7bcf
AASIF <aasif212233@gmail.com>
2026-05-26 08:43:05 +0000

    javascript

---
+++ javascript/project/intro.js
@@ -0,0 +1,3 @@
+console.log("welcome to javascript");
+//javascript=> JavaScript ek high-level, interpreted scripting language hai jo websites ko dynamic aur
+            //  interactive banane ke liye use hoti hai
